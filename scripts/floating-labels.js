9dd9c98e22c8e340e962bf283fb7929bc84b83d4
Michael Szeto <szetomj@gmail.com>
2017-05-02 22:12:53 +1200

    Removed unneeded JS files
    Removed unneeded JS files

--- scripts/floating-labels.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-    var emailFormSelect = $('.email-form');
-    var labelEmail = $('label.email');
-
-    emailFormSelect.on("blur focus", function(){
-        labelEmail.toggleClass('float-up');
-    });	
-});
